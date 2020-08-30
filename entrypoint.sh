77e7d2b574641191b4484b283fdc4d4eea717136
Mavi <mavi.jimenez@code.seat>
2020-08-30 16:25:25 +0200

    foobar

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/sh -l
 echo "Waiting for mysql service is up..."
 i=0
-#while ! nc -z outbound-mysql 3306; do sleep 1;
-while true; do sleep 1;
-echo aa
-if [[ $i -gt 5 ]]
-then
-  exit 1
-fi
-i=$((i+1))
-echo fo $i
-
-
+while ! nc -z outbound-mysql 3306; do sleep 1;
+  if [[ $i -gt 30 ]]; then exit 1; fi; i=$((i+1));
 done;
--- test-action/entrypoint.sh
+++ test-action/entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh -l
 echo "Waiting for mysql service is up..."
 i=0
-#while ! nc -z outbound-mysql 3306;
-while true; do sleep 1;
-if [[ i -gt 5 ]]
-then
-  exit 1
-fi
-((i+=1))
+while ! nc -z outbound-mysql 3306; do sleep 1;
+  if [[ $i -gt 30 ]]; then exit 1; fi; i=$((i+1));
 done;
-#./mvnw test
+
+./mvnw test
