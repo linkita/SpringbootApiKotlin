04bc60196152a801aec0441ec326e1bb59843c56
Mavi <mavi.jimenez@code.seat>
2020-08-30 12:28:04 +0200

    Create test actions

--- test-action/entrypoint.sh
+++ test-action/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
-echo "Waiting for mysql service is up"
+echo "Waiting for mysql service is up..."
 while ! nc -z outbound-mysql 3306; do sleep 1; done;
-docker exec -it outbound-api ./mvnw test
+#/bin/sh
+./mvnw test
 echo $?
