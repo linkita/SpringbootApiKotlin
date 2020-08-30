205174a4ea06120a9f4285a28d6fdad99cdab90d
Mavi <mavi.jimenez@code.seat>
2020-08-30 15:32:53 +0200

    Improve entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -l
 echo "Waiting for mysql service is up"
-while ! nc -z outbound-mysql 3306; do sleep 1; done;
+while ! nc -z outbound-mysql 3306; do sleep 1;
+if [[ i -gt 15 ]]; then exit 1; fi
+((i+=1));
+done;
 /bin/sh
